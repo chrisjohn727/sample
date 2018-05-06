609b9016e087b3c5c87b475f2f6ccd8861a1b9b8
chrisjohn727 <38350913+chrisjohn727@users.noreply.github.com>
2018-05-06 14:07:41 +0530

    Create app >  instruments > newInstrument > newInstrumentController.ts

---
+++ app >  instruments > newInstrument > newInstrumentController.ts
@@ -0,0 +1,47 @@
+app >  instruments > newInstrument > newInstrumentController.ts
+It defines how the data is displayed. You can also use the file to:
+ •	create your property panel structure,
+ •	define custom/private properties (aside from the ones in the ‘config’) 
+
+module PoseidonWidgetsPackage {
+    "use strict";
+
+    interface INewInstrumentConfig {
+        displayText: string;
+    }
+
+    export interface INewInstrumentConfigScope extends Poseidon.DisplayFramework.IWidgetContentScope<newInstrumentConfig> {
+        config: newInstrumentConfig;
+    }
+
+    export class NewInstrumentController extends Poseidon.DisplayFramework.WidgetPropertyBaseController {
+        private displayText: string;
+
+        static $inject = ['$scope'];
+
+        constructor(private scope) {
+            super(scope);
+            this.init(scope);
+        }
+
+        private init(scope: INewInstrumentConfigScope): void {
+            if (scope.config) {
+
+            }
+
+            if (scope.header) {
+                this.setConfigurationNodes(this.createConfigurationNodes(scope));
+            }
+        }
+
+        private createConfigurationNodes(scope: INewInstrumentConfigScope): Poseidon.DisplayFramework.IWidgetPropertyConfigurationNode[] {
+            var nodes: Poseidon.DisplayFramework.IWidgetPropertyConfigurationNode[] = [];
+
+            var generalNode = new Poseidon.DisplayFramework.WidgetPropertyConfigurationNode('9725be3c-06a1-4b33-b136-1fcef62d86f4',
+                'propcontext.newWidget.general', 'General', () => { });
+            nodes.push(generalNode);
+
+            return nodes;
+        }
+    }
+};
