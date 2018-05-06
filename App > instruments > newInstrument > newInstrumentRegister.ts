55df8d09ec92e472455fc084cf8e9ca3c4a62d84
chrisjohn727 <38350913+chrisjohn727@users.noreply.github.com>
2018-05-06 14:27:40 +0530

    Create App > instruments > newInstrument > newInstrumentRegister.ts

---
+++ App > instruments > newInstrument > newInstrumentRegister.ts
@@ -0,0 +1,29 @@
+App > instruments > newInstrument > newInstrumentRegister.ts
+It is used to bootstrap the widget and define its meta properties
+
+module PoseidonWidgetsPackage {
+
+    angular.module('PoseidonWidgetsPackage').config(newInstrumentConfigFunction);
+
+    newInstrumentConfigFunction.$inject = ['displayWidgetsProvider'];
+
+    function newInstrumentConfigFunction(dashboardProvider: Poseidon.DisplayFramework.IDashboardWidgetsProvider): void {
+        var instrument: Poseidon.DisplayFramework.IWidget =
+            {
+                title: 'New Instrument Test',
+                description: 'Automatically created instrument Test',
+                category: 'Custom2',
+                template: '<new-instrument></new-instrument>',
+                //templateUrl: '/PoseidonWidgetsPackage/instruments/newInstrument/newInstrument.html',
+                controller: 'NewInstrumentDataController',
+                controllerAs: 'vm',
+                reload: false,
+                config: new newInstrumentDataConfig(),
+                edit: {
+                    templateUrl: '/PoseidonWidgetsPackage/app/instruments/newInstrument/newInstrumentEdit.html'
+                }
+            }
+
+        dashboardProvider.registerWidget('NewInstrument', instrument);
+    }
+}
